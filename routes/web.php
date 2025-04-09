da372af13b5c7465fac59ab30bd113f289ff5abd
Lawrence Bisnar <labisnar21@gmail.com>
2025-04-09 14:11:39 +0800

    prefix routing

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UiController;
 
-Route::get('/login', [UiController::class, 'login'])->name('patientUI.login');
-Route::get('/register', [UiController::class, 'register'])->name('patientUI.register');
-Route::get('/profile', [UiController::class, 'profile'])->name('patientUI.profile');
-Route::get('/welcome', [UiController::class, 'welcome'])->name('patientUI.welcome');
+Route::prefix('/')->name('patientUI.')->group(function () {
+    Route::get('/login', [UiController::class, 'login'])->name('login');
+    Route::get('/register', [UiController::class, 'register'])->name('register');
+    Route::get('/profile', [UiController::class, 'profile'])->name('profile');
+    Route::get('/welcome', [UiController::class, 'welcome'])->name('welcome');
+});
 
-Route::get('/dashboard', [UiController::class, 'dashboard'])->name('adminUI.dashboard');
-Route::get('/dentist', [UiController::class, 'dentist'])->name('adminUI.dentist');
-Route::get('/staff', [UiController::class, 'staff'])->name('adminUI.staff');
+
+Route::prefix('/')->name('adminUI.')->group(function () {
+    Route::get('/dashboard', [UiController::class, 'dashboard'])->name('dashboard');
+    Route::get('/dentist', [UiController::class, 'dentist'])->name('dentist');
+    Route::get('/staff', [UiController::class, 'staff'])->name('staff');
+});
 
