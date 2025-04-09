c838dda621a076269cabc98b1a8cbfe08d78ee0e
dharyl <dharyljay1@gmail.com>
2025-04-09 22:13:06 +0800

    crud(1)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\SupplyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// project supplies
+Route::get('/supply', [SupplyController::class, 'index'])->name('staffs.supply');
+
+Route::post('/staffs/supply', [SupplyController::class, 'store'])->name('staffs.supply.store');
+Route::put('/supply/{supply}/update', [SupplyController::class, 'update'])->name('supply.update');
+Route::delete('/supply/{supply}/destroy', [SupplyController::class, 'destroy'])->name('supply.destroy');
